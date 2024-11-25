fae6f661e37c0d1cf73db56599e4f8fce31cbd54
zreonx <zrodayrit@gmail.com>
2024-11-25 14:07:01 +0800

    added responsive images and auth routing technique, and aslo the prisma setup

---
+++ app/(dashboard)/auth/[[...sign-in]]/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignInPage({params}) {
+  console.log(params);
+  
+  return (
+    <h1 className='text-7xl'>
+      SignInPage
+    </h1>
+  )
+}
+
+export default SignInPage
