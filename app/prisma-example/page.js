dd158be85f2a0987fd104b8e0508c36dbdf9efd3
zreonx <zrodayrit@gmail.com>
2024-11-28 11:26:59 +0800

    added force dynamic on static page, as well as added env for mongo db subject for hosting

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,11 +1,14 @@
+import TaskList from '@/components/TaskList';
 import prisma from '@/utils/db';
 
 const prismaHandlers = async () => {
-  await prisma.task.create({
-    data: {
-      content: 'wake up'
-    }
-  })
+  console.log('prisma example');
+  
+  // await prisma.task.create({
+  //   data: {
+  //     content: 'wake up'
+  //   }
+  // })
 
   const allTasks = await prisma.task.findMany({
     orderBy: {
@@ -20,12 +23,7 @@ const PrismaExamplePage = async () => {
   const tasks = await prismaHandlers();
   return (
     <div>
-      <h1 className="text-7xl">Prisma Example Page</h1>
-      {
-        tasks.map((task) => {
-          return <h2 key={task.id} className='text-xl py-2'>😁 {task.content}</h2>
-        })
-      }
+      <TaskList />
     </div>
   );
 };
--- app/tasks/page.js
+++ app/tasks/page.js
@@ -2,6 +2,8 @@ import TaskForm from "@/components/TaskForm";
 import TaskFormCustom from "@/components/TaskFormCustom";
 import TaskList from "@/components/TaskList";
 
+export const dynamic = 'force-dynamic';
+
 const TaskPage = () => {
   return (
     <div className="max-w-lg">
